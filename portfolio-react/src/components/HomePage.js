398b94208ac7f16e9e9fe0a966baa46c93b3807c
David Gold <davidgold@hey.com>
2020-09-28 23:00:01 -0400

    got hover gif in place

--- portfolio-react/src/components/HomePage.js
+++ portfolio-react/src/components/HomePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./homepage/Header";
+import HomepageBody from "./homepage/HomepageBody";
 
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <HomepageBody />
+    </>
+  );
 };
 
 export default HomePage;
---
+++ portfolio-react/src/components/homepage/HomepageBody.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const HomepageBody = () => {
+  const [overImage, setOverImage] = useState(false);
+
+  return (
+    <>
+      <body className="homepage-body-container">
+        {!overImage && (
+          <img
+            src={require("/Users/davidgold/Documents/lambdaSchool/projects/personal/portfolio-page/portfolio-react/src/assets/west-side-hwy-img.png")}
+            alt="Funny GIF by the website creator"
+            className="web-owner-gif"
+            onMouseEnter={() => setOverImage(true)}
+          />
+        )}
+
+        {overImage && (
+          <img
+            src={require("/Users/davidgold/Documents/lambdaSchool/projects/personal/portfolio-page/portfolio-react/src/assets/west-side-hwy.gif")}
+            alt="Funny GIF by the website creator"
+            className="web-owner-gif"
+            onMouseLeave={() => setOverImage(false)}
+          />
+        )}
+      </body>
+    </>
+  );
+};
+
+export default HomepageBody;
