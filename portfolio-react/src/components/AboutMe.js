471511a418bd9d34b77d998bbd958c0fbbf3d7c4
David Gold <davidgold@hey.com>
2020-09-30 15:38:38 -0400

    flexboxed the about me page

--- portfolio-react/src/components/AboutMe.js
+++ portfolio-react/src/components/AboutMe.js
@@ -3,22 +3,23 @@ import React from "react";
 const AboutMe = () => {
   return (
     <section className="about-me-page-container">
-      <div className="about-me-image-container">
-        <img
-          src={require("/Users/davidgold/Documents/lambdaSchool/projects/personal/portfolio-page/portfolio-react/src/assets/2020-06-23-7531-ANIMATION (1).png")}
-          alt="pic of David"
-          className="about-me-image"
-        />
-      </div>
+      <img
+        src={require("/Users/davidgold/Documents/lambdaSchool/projects/personal/portfolio-page/portfolio-react/src/assets/2020-06-23-7531-ANIMATION (1).png")}
+        alt="pic of David"
+        className="about-me-image"
+      />
       <div className="about-me-text-container">
-        <h6>01. About Me</h6>
-        <h6>Hi, I'm David Gold</h6>
-        <p>
-          I’m the co-founder of The Rotation, and ocassional engineer, product
-          designer, photographer, and DJ in New York City.
-        </p>
-        <span>Instagram / Twitter / Email</span>
+        <h1>01. About Me</h1>
+        <div>
+          <h2>Hi, I'm David Gold</h2>
+          <p>
+            I’m the co-founder of The Rotation, and ocassional engineer, product
+            designer, photographer, and DJ in New York City.
+          </p>
+        </div>
+        <h4>Instagram / Twitter / Email</h4>
       </div>
+      <div className="description-page-gradient"></div>
     </section>
   );
 };
